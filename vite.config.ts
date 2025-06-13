bdc2f74f13d481873ecbdea6efddd322dc781e0d
Kotaro Kibe <kibe@e3sys.co.jp>
2025-06-13 18:13:54 +0900

    vite.config.tsにベースURLを'/fortune-telling-chat/'に再設定しました。

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fortune-telling-chat/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
